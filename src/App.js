50ae19e871a7b2961e03654fe898a06a6a834d39
kasakvaish <kasakvaish02@gmail.com>
2022-08-02 01:21:38 +0530

    simpleapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './card.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
